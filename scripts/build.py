d837392fd32dde82f4759486898c6d495a46b8fa
Tekiter <tekiter8@gmail.com>
2021-06-09 20:18:19 +0900

    Create build script

--- scripts/build.py
+++ scripts/build.py
@@ -7,45 +7,73 @@ import glob
 import os
 import re
 
+from shutil import copyfile, rmtree
+
+SOURCE_PATTERN = r'.+.py$'
+
 
 def build(src_dir, dist_dir):
     move_to_root_path()
-    create_dist_dir(dist_dir)
+    clean_dir(dist_dir)
+    create_dir(dist_dir)
 
-    # src = os.path.join(root, src_dir)
-    # dist = os.path.join(root, dist_dir)
-
-    pattern = re.compile(r'.+.py$')
-    for root, _, files in os.walk(src_dir):
-        for f in files:
-            if pattern.match(f):
-                print(root, f)
+    copy_files(src_dir, dist_dir)
 
 
-def create_dist_dir(dist_dir):
-    path = dist_dir
+def clean_dir(dir_path):
+    if os.path.isdir(dir_path):
+        rmtree(dir_path)
+
+
+def create_dir(dir_path):
+    path = dir_path
     try:
         os.mkdir(path)
     except OSError:
         if not os.path.isdir(path):
-            raise
-
-
-def get_root_path():
-    current_dir_path = os.path.dirname(__file__)
-    parent_dir_path = os.path.dirname(current_dir_path)
-
-    return parent_dir_path
+            raise OSError('Creating directory "{}" failed.'.join(dir_path))
 
 
 def move_to_root_path():
     os.chdir(get_root_path())
 
 
-def get_source_files():
-    pattern = 'src/**/*.py'
-    files = glob.glob(pattern, recursive=True)
-    return files
+def get_root_path():
+    current_dir_path = os.path.dirname(os.path.abspath(__file__))
+    parent_dir_path = os.path.dirname(current_dir_path)
+
+    return parent_dir_path
+
+
+def copy_files(src_dir, dist_dir):
+    for dir, filename in source_files(src_dir):
+
+        target_dir = source_path_to_target_dir(dir, src_dir, dist_dir)
+
+        src_path = os.path.join(dir, filename)
+        target_path = os.path.join(target_dir, filename)
+
+        create_dir(target_dir)
+        copyfile(src_path, target_path)
+
+        print("{} >> {}".format(src_path, target_path))
+
+
+def source_files(src_dir):
+    pattern = re.compile(SOURCE_PATTERN)
+    for dir, _, files in os.walk(src_dir):
+        for filename in files:
+            if pattern.match(filename):
+                yield (dir, filename)
+
+
+def source_path_to_target_dir(source_path, src_dir, dist_dir):
+    frag_dir = get_path_without_head_dir(source_path, src_dir)
+    return os.path.join(dist_dir, frag_dir)
+
+
+def get_path_without_head_dir(path, head):
+    return path[len(head)+1:]
 
 
 if __name__ == '__main__':
