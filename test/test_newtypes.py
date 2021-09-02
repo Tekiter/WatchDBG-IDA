c03897aafb45c3aa5cf3e1a803800c965e353b03
Tekiter <tekiter8@gmail.com>
2021-09-02 23:58:47 +0900

    Change test

--- test/test_newtypes.py
+++ test/test_newtypes.py
@@ -14,3 +14,12 @@ class SignedIntTest(unittest.TestCase):
 
         signed = types.SignedInt(4)
         self.assertEqual(signed.name, "Int32")
+
+    def test_to_str_invalid_size(self):
+        signed = types.SignedInt(2)
+        self.assertRaises(types.DataSizeNotMatchError,
+                          lambda: signed.to_str(b'\x12\x34\x56'))
+
+    def test_to_str(self):
+        signed = types.SignedInt(2)
+        self.assertEqual(signed.to_str(b'\x34\x12'), str(0x1234))
